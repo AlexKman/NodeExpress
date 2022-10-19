caea7ded14d74c15251903f6c8b8aef2ae04b837
Alex <keeley.lp@gmail.com>
2022-10-19 12:47:43 +0100

    Added path util for root directory

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,12 @@ const path = require("path");
 
 const express = require("express");
 
+const root = require("../util/path");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(root, "views", "shop.html"));
 });
 
 module.exports = router;
